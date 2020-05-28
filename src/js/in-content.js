68314c048489fd9f133cf9ef7b673002755c614d
Adrian Smith (VivaLaPanda) <adrian@smithdev.io>
2020-05-27 17:20:13 -0700

    initial commit

---
+++ src/js/in-content.js
@@ -0,0 +1,8 @@
+/* in-content.js
+*
+* This file has an example on how to communicate with other parts of the extension through a long lived connection (port) and also through short lived connections (chrome.runtime.sendMessage).
+*
+* Note that in this scenario the port is open from the popup, but other extensions may open it from the background page or not even have either background.js or popup.js.
+* */
+
+
